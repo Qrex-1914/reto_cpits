17b8b9ab32d1d023a8c516bdbf323cb7608a38f9
karinandrade <karinandrade97@outlook.com>
2022-11-15 00:05:33 -0500

    Carpetas Reto CPITS

---
+++ app.py
@@ -0,0 +1,12 @@
+from random import randrange
+from flask import Flask, render_template, request
+import hashlib
+#import controlador
+from datetime import datetime
+#import envio_correo
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
